38de0625b5ad1cbb3b006e62465281e7c4fb059b
Mateo Gandarillas <mateoganav@gmail.com>
2025-02-21 19:22:45 -0400

    Feat:Convertir 2 a romano

--- src/romano.js
+++ src/romano.js
@@ -1,5 +1,12 @@
 function romano(a) {
-  if (a === 1) return "I";
+  if (a === 1) 
+    {
+      return "I";
+    }
+  if (a === 2) 
+    {
+      return "II";
+    }
 }
 
 export default romano;
--- src/romanos.test.js
+++ src/romanos.test.js
@@ -4,4 +4,9 @@ test("Convierte 1 a I", () => {
   expect(romano(1)).toEqual("I");
 });
 
+test("Convierte 2 a II", () => {
+  expect(romano(2)).toEqual("II");
+});
+
+
 
